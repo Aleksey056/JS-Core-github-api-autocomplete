df9dd647049b855dbd9d85ade8fe5e7f5b7fe808
Aleksey Krendukov <aikrendukov@yandex.ru>
2025-07-30 13:31:09 +0500

    finish

--- main.js
+++ main.js
@@ -1,36 +1,67 @@
 const searchField = document.querySelector('#search')
+const cardTmpl = document.querySelector('#card')
+const field = document.querySelector('.out')
+const addition = document.querySelector('#addition')
 
 
+const addCard = (item) => {
+	searchField.value = ''
+	addition.innerHTML = ''
+	const card = cardTmpl.content.cloneNode(true)
+	card.querySelector('.alternative-name').textContent = `Name: ${item.name}`
+	card.querySelector('.alternative-owner').textContent = `Owner: ${item.owner.login}`
+	card.querySelector('.alternative-stars').textContent = `Stars: ${item.stargazers_count}`
+	card.querySelector('.card-button').addEventListener('click', (evt) => {
+		evt.target.parentNode.remove()
+	})
+	field.append(card)
 
+}
 
-
-const getRepositiors = async (searchItem) => {
-	return await fetch('https://api.github.com/search/repositories?q=${searchItem}', {
+const getRepositoriess = async (searchItem) => {
+	return await fetch(`https://api.github.com/search/repositories?q=${searchItem}`, {
 		headers: {
 			'X-GitHub-Api-Version': '2022-11-28'
 		}
 	})
 		.then(response => {
 			if (response.ok) {
-				response.json().then(repos => {
-					autocomplete.textContent = ''
-					const items = repos.items.slice(0, 5)
+				response.json().then(res => {
+					addition.textContent = ''
+					const items = res.items.slice(0, 5)
 					if (items.length === 0) {
-						autocomplete.textContent = '<p class="no-results">No results...</p>'
+						addition.innerHTML = '<p class="no-results">Такой репозиторий не найден!</p>'
 					} else {
 						items.forEach(item => {
-							const choice = document.createElement('p')
-							choice.className = 'choice'
-							choice.textContent = `${item.name}`
-							choice.addEventListener('click', () => addCard(item))
-							autocomplete.append(choice)
+							const alternative = document.createElement('p')
+							alternative.className = 'alternative'
+							alternative.textContent = `${item.name}`
+							alternative.addEventListener('click', () => addCard(item))
+							addition.append(alternative)
 						})
 					}
 				})
 			} else {
-				autocomplete.innerHTML = '<p class="no-results">Try again...</p>'
+				addition.innerHTML = '<p class="no-results">Я за вами не успел, попробуйте снова <br> ввести название репозитория</p>'
 			}
 		})
 }
 
+const debounce = (fn, delay) => {
+	let timer;
+	return function (...args) {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			fn.apply(this, args)
+		}, delay)
+	}
+};
 
+const debounceGetRepositoriess = debounce(getRepositoriess, 500)
+
+searchField.addEventListener('input', () => {
+	if (searchField.value[0] === ' ') {
+		return
+	}
+	debounceGetRepositoriess(searchField.value.trim())
+})
